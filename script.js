281fc1ff31c2bf000c5df8f3fa16fec026fec309
SarthakJoshi21 <157214236+SarthakJoshi21@users.noreply.github.com>
2024-10-19 13:28:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const student={
-	name : 'Sarthak'
-}
-Object.prototype.getkeys=function(){
-	return Object.keys(this);
-}
+const student = {
+  name: "Sarthak"
+};
+
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
 console.log(student.getKeys());
